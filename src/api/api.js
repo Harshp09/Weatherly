e4dc3d2e96a1d5a76e5ed8409340aab377ad67f1
Harshp09 <harsh9503pandey@gmail.com>
2024-05-19 12:10:20 +0530

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '3ccd241f7a8dd3e957e5bfb048b3b0d4';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
